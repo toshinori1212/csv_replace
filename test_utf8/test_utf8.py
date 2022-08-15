c739b9ab2a7abc41667091972683a81f161b5fb3
toshinori1212 <64344850+toshinori1212@users.noreply.github.com>
2022-08-16 05:43:54 +0900

    aa
    aaa

---
+++ test_utf8/test_utf8.py
@@ -0,0 +1,19 @@
+import csv
+import numpy as np
+import glob
+ 
+list_path = glob.glob('./*.csv');
+
+for path in list_path:
+    with open(path, mode='r', newline='', encoding='utf_8') as f:
+        csv_reader = csv.reader(f)
+        read_data = [row for row in csv_reader]
+
+    ans_data = read_data
+    ans_data = np.char.replace(ans_data, "\r\n", "、")
+    ans_data = np.char.replace(ans_data, "\n", "、")
+    ans_data = np.char.replace(ans_data, ",", "、")
+
+    with open('./utf_8.csv', "w", newline="") as f:
+        writer = csv.writer(f, lineterminator='\n')
+        writer.writerows(ans_data)
