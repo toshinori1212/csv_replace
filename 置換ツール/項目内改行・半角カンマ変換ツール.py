baa2e3d08c102756cc96dad9d7494e98fb17d2b9
toshinori1212 <64344850+toshinori1212@users.noreply.github.com>
2022-08-16 14:04:00 +0900

    改修
    改修

--- 置換ツール/項目内改行・半角カンマ変換ツール.py
+++ 置換ツール/項目内改行・半角カンマ変換ツール.py
@@ -32,6 +32,26 @@ for path in list_path:
             writer1.writerows(ans_data)
             f.close()
     
+    if result['encoding'] == 'SHIFT_JIS':
+        
+        with open(path, mode='r', newline='', encoding='SHIFT_JIS') as f:
+            csv_reader = reader(f)
+            read_data = [row for row in csv_reader]
+            f.close()
+
+        ans_data = read_data
+        ans_data = char.replace(ans_data, "\r\n", "、")
+        ans_data = char.replace(ans_data, "\n", "、")
+        ans_data = char.replace(ans_data, ",", "、")
+        
+        filename = path.replace('.csv','')
+        filename = filename + '_置換済み.csv'
+
+        with open(filename, "w", newline="") as f:
+            writer2 = writer(f, lineterminator='\n')
+            writer2.writerows(ans_data)
+            f.close()
+            
     if result['encoding'] == 'utf-8':
 
         with open(path, mode='r', newline='', encoding='utf_8') as f:
@@ -48,6 +68,6 @@ for path in list_path:
         filename = filename + '_置換済み.csv'
 
         with open(filename, "w", newline="", encoding='utf_8') as f:
-            writer2 = writer(f, lineterminator='\n')
-            writer2.writerows(ans_data)
+            writer3 = writer(f, lineterminator='\n')
+            writer3.writerows(ans_data)
             f.close()
